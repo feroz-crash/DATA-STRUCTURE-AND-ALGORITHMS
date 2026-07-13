5715ca7b3aacf0d8fc64dcf023ff896a0871f697
feroz-crash <rose17291729@gmail.com>
2026-07-13 09:50:36 +0530

    .

--- patterns/pattern22.cpp
+++ patterns/pattern22.cpp
@@ -7,6 +7,8 @@ void pattern22(int n){
         for(int j=0; j<2*n-1; j++){
             top=i;
             left=j;
+
+            
             bottom=2*n-2-i;
             right=2*n-2-j;
             mindist=min(min(top,bottom),min(left,right));
