4f20d9c64f7572cf6fca4ce5d8cf544da939a0a6
feroz-crash <rose17291729@gmail.com>
2026-08-19 17:28:52 +0530

    Fix typo in variable assignment in middleNode function

--- linked list/876. middle of the linked list.py
+++ linked list/876. middle of the linked list.py
@@ -8,7 +8,7 @@ class ListNode:
 
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = hea
+        slow = head
         fast = head
         while fast and fast.next:
             fast = fast.next.next
